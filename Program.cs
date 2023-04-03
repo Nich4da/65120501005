16d494e1e1512a931b8fd403dd9db498990ee47a
nicha <marnichacha27@gmail.com>
2023-04-03 10:34:04 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double inputNumber;
+        string inputString;
+
+        while (true) {
+            Console.Write("Enter a number or type 'End' to stop: ");
+            inputString = Console.ReadLine();
+
+            //Check if user entered 'End'
+            if (inputString == "End") {
+                break;
+            }
+
+            //Parse user input as a number
+            if (double.TryParse(inputString, out inputNumber)) {
+                Console.WriteLine("You entered: " + inputNumber);
+            } else {
+                Console.WriteLine("Invalid input");
+            }
+        }
+        Console.WriteLine("Ended");
+    }
+}
